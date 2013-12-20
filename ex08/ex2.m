69977cfc602bf09f28bcc126fb94db03cb63d77c
LindaLeidig <Lilei123@aol.com>
2013-12-20 17:54:25 +0100

    start of the implementation of exercise 2

---
+++ ex08/ex2.m
@@ -0,0 +1,29 @@
+l = 0;
+
+X = [-0.8372, -0.4558, 0.6902, 0.1114, -0.4678];
+Y = [-1.1414, -1.5286, -1.1893, -1.9021, -1.5595];
+
+Xstar = [-0.5, 0.5];
+YstarMean = zeros(1,2);
+
+% create K
+K = zeros(size(X,2), size(X,2));
+for i = 1:size(X,2)
+    for j = 1:size(X,2)
+        K(i,j) = kernel(X(i),X(j), l);
+    end
+end
+
+% compute the mean value ystar for each xstar
+for x = 1:size(Xstar,2)
+    
+    Kstar = zeros(size(X,2), 1);
+    for i = 1:size(X,2)
+        Kstar(i,1) = kernel(X(i), Xstar(x), l);
+    end
+    
+    KstarStar = kernel(Xstar(x), Xstar(x), l);
+    
+    YstarMean(x) = Kstar'*inv(K)* normpdf(Xstar(x), 0, KstarStar);
+end
+
---
+++ ex08/kernel.m
@@ -0,0 +1,10 @@
+function [ w ] = kernel( x1, x2, l )
+
+    sigmaF = 1;
+    sigmaN = 0.5;
+    delta = 0;
+    
+    w = sigmaF^2 * exp(-1/(2*l^2)*(x1-x2)^2) + sigmaN^2*delta;
+
+end
+
